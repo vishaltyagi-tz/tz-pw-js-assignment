058b5c6fe6c314a90d5e0fe15f33a59741daf789
Avinash Singh <avinash.singh@taazaa.com>
2026-08-05 08:18:59 -0400

    Added Avinash Week 01 assignment

---
+++ Sessions/Week-01-Environment-Setup-First-Line-of-Code/Avinash-Singh/Assignment.js
@@ -0,0 +1,16 @@
+console.log("Welcome to node");
+let name = "Avinash";
+let age = 32;
+let city = "Amritsar";
+console.log("My name is", name);
+console.log("My age is", age);
+console.log("I live in", city);
+
+// Observe execution order
+console.log(10);
+console.log(20);
+console.log(30);
+
+//Output by variable name
+let company = "OpenAI";
+console.log(company);
