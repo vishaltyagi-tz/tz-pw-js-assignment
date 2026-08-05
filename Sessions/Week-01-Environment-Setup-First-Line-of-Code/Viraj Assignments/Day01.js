b08d0581c035ffac6a1ac11c2db48f71d33fa832
Viraaj9 <viraj.mandavkar@taazaa.com>
2026-08-05 16:54:51 +0530

    Viraj Assignment

---
+++ Sessions/Week-01-Environment-Setup-First-Line-of-Code/Viraj Assignments/Day01.js
@@ -0,0 +1,39 @@
+console.log("Hello Viraj");
+console.log("Viraj");
+console.log ("Hello Word")
+ 
+console.log ("Welcome")
+ 
+console.log (100)
+ 
+console.log (true)
+ 
+console.log ("Age:",25)
+ 
+let name = "Viraj";
+console.log(name);
+ 
+console.log("First");
+console.log("Second");
+console.log("Third");
+ 
+console.log("Program started");
+let age =30 ;
+console.log(age);
+console.log("program finished");
+ 
+///////////////// Assignmet ///////////////
+ 
+console.log ("Welcome");
+ 
+console.log ("Name : Viraj");
+console.log ("Age: 28");
+console.log ("City: Pune");
+ 
+console.log(10);
+console.log(20);
+console.log(30);
+ 
+console.log("Start");
+console.log("Learning Node");
+console.log("End");
