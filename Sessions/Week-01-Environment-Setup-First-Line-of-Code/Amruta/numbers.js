f642ae76a4108cc5d7f9ce9c4fc4b564dd050402
amrutazargad <amruta.zargad@taazaa.com>
2026-08-07 16:57:43 +0530

    week 1 assignment

---
+++ Sessions/Week-01-Environment-Setup-First-Line-of-Code/Amruta/numbers.js
@@ -0,0 +1,17 @@
+
+for(i= 1; i<6; i++ ){
+    console.log(i)
+// 1,2,3,4,5
+}
+
+// Create a variable named courseName, assign it the value Node.js Training, and print it using console.log(). 
+let courseName = "Node.js"
+console.log(courseName)
+
+// Create variables for firstName, lastName, and city, then print them in a readable format. 
+let firstName = "Amruta"
+let lastName = "Zargad"
+let city = "Pune"
+console.log("My name is " + firstName + " " +lastName)
+console.log("I am from" + " "+city)
+
---
+++ Sessions/Week-01-Environment-Setup-First-Line-of-Code/Amruta/sequence.js
@@ -0,0 +1,4 @@
+let a = "Step 1"
+let b = "step 2"
+let c = "Step 3"
+console.log(a + "\n" + b + "\n" + c)
