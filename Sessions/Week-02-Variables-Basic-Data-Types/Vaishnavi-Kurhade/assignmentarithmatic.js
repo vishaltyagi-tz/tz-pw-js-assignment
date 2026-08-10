feef70db3ca6a6a87194de425fa7a969c26988d7
VaishnaviK315 <vaishnavi.kurhade@taazaa.com>
2026-08-10 14:20:31 +0530

    assignment

--- Sessions/Week-02-Variables-Basic-Data-Types/Vaishnavi-Kurhade/assignmentarithmatic.js
+++ Sessions/Week-02-Variables-Basic-Data-Types/Vaishnavi-Kurhade/assignmentarithmatic.js
@@ -1,14 +1,21 @@
-let num1 = 36
-let num2 = 5
+let num1 = 15
+
+let num2 = 15
+
 let sum, difference, product, division;
-sum = num1 + num2 
+
+sum = num1 + num2
+
 console.log (num1, "+", num2, "=", sum)
 
 difference = num1 - num2
+
 console.log (num1, "-", num2, "=", difference)
 
 product = num1 * num2
+
 console.log (num1, "*", num2, "=", product)
 
 division = num1 / num2
+
 console.log (num1, "/", num2, "=", division)
