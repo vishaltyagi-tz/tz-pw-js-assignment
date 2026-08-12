d7c4784c68df505226557afa730913e1191276b7
AwantiTaazaa <awanti.mahadik@taazaa.com>
2026-08-12 14:30:00 +0530

    Awanti assignment 2

---
+++ Sessions/Week-01-Environment-Setup-First-Line-of-Code/Awanti-Mahadik/favourite.js
@@ -0,0 +1,7 @@
+let CourseName = "Node.js Training";
+console.log("My favourite course is: " + CourseName);
+let firstName = "Awanti";
+let lastName = "Mahadik";
+let City = "Pune";
+console.log("My name is: " + firstName + " " + lastName);
+console.log("I live in: " + City);
