fe5f404712f837f89cbdba5822abf9a3f169a191
Shubham Saraswat <shubham.saraswat@taazaa.com>
2026-08-12 03:50:43 -0400

    Week 2 assignment

---
+++ Sessions/Week-02-Variables-Basic-Data-Types/Shubham-Saraswat/assignment/full-name.js
@@ -0,0 +1,5 @@
+let firstName = "Shubham";
+let lastName = "Saraswat";
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
---
+++ Sessions/Week-02-Variables-Basic-Data-Types/Shubham-Saraswat/assignment/numbers.js
@@ -0,0 +1,7 @@
+let num1 = 20;
+let num2 = 8;
+
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Division:", num1 / num2);
