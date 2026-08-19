2ed0e108b04cccd803f01afb52eda0c680622a2e
Aishwarya-sh-ops <aishwarya.thakur@taazaa.com>
2026-08-19 13:55:44 +0530

    assingmnet 2

---
+++ Sessions/Week-02-Variables-Basic-Data-Types/Aishwarya-Thakur/age.js
@@ -0,0 +1,3 @@
+let age= 28
+let canVote=age >=28
+console.log(canVote);
---
+++ Sessions/Week-02-Variables-Basic-Data-Types/Aishwarya-Thakur/week2.js
@@ -0,0 +1,10 @@
+
+let num1= 20;
+let num2= 10;
+console.log ("difference=" + " " + (num1 - num2))
+
+product = num1 * num2
+console.log (num1 * num2)
+
+division = num1 / num2
+console.log (num1/num2)
